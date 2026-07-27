f099a9bbb83266a757bf0fb12d2f95f50b2d0a53
Nick Yaremovich <nickyaremovich@gmail.com>
2026-07-27 16:50:12 -0600

    add: book constructor

---
+++ app.js
@@ -0,0 +1,10 @@
+const library = [];
+
+// book constructor
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read
+};
