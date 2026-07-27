763bc338b797001b44dc3923a6c45b2ebce8d7c4
Nick Yaremovich <nickyaremovich@gmail.com>
2026-07-27 17:35:52 -0600

    add test button for DOM

--- app.js
+++ app.js
@@ -14,4 +14,20 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     library.push(book);
-}
+};
+
+// hardcode a couple books
+
+addBookToLibrary('The Count of Monte Cristo', 'Alexandre Dumas', 1232, true);
+addBookToLibrary('East of Eden', 'John Steinbeck', 607, true);
+// log to see library 
+console.log(library);
+
+
+
+
+
+// DOM elements
+const addBook = document.getElementById('add-book');
+// event listener to open book form. modal or side dialog?
+addBook.addEventListener('click', openForm());
